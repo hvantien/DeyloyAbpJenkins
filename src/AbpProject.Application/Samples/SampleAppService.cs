44147638052092aeb5b21dc9c1ce98e5b6933186
tienhv <tienhv@greenglobal.vn>
2021-06-09 23:50:01 +0700

    aaa

--- src/AbpProject.Application/Samples/SampleAppService.cs
+++ src/AbpProject.Application/Samples/SampleAppService.cs
@@ -10,7 +10,7 @@ namespace AbpProject.Samples
             return Task.FromResult(
                 new SampleDto
                 {
-                    Value = 461
+                    Value = 4611
                 }
             );
         }
