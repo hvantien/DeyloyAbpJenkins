39563c17796e9ca7599d29a6cf18f7c93d815d9a
tienhv <tienhv@greenglobal.vn>
2021-06-09 23:39:07 +0700

    Test CI 2

--- src/AbpProject.Application/Samples/SampleAppService.cs
+++ src/AbpProject.Application/Samples/SampleAppService.cs
@@ -10,7 +10,7 @@ namespace AbpProject.Samples
             return Task.FromResult(
                 new SampleDto
                 {
-                    Value = 43
+                    Value = 45
                 }
             );
         }
